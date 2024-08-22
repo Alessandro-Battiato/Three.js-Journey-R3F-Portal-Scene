f9f7c272da9e6156f13e08b3231ea7980d6dedd6
alessandrobattiato <alessandroDev@outlook.it>
2024-08-22 19:34:33 +0200

    fix: tonemapping related issue

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
     <Canvas
+        flat // this lets us use the exact colors we used on blender for our model because Blender already uses tone mapping for colors but Three.js does the same which is usually accepted but not on this case since Blender already did it, so this prop fixes the issue
         camera={{
             fov: 45,
             near: 0.1,
